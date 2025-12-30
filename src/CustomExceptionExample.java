a59de849df6ab651125ac959cdce0521619100dd
darisasubhash <subbusubbu8193@gmail.com>
2025-12-30 13:00:34 +0530

    refactoring the custom exception

--- src/CustomExceptionExample.java
+++ src/CustomExceptionExample.java
@@ -1,40 +1,27 @@
 import java.util.Scanner;
 
-class UnderAgeException extends Exception{
-    @Override
-    public String getMessage() {
-        return "You are not eligible because age is too low";
+class AgeException extends RuntimeException{
+    public AgeException(String message) {
+        super(message);
     }
 }
-class OverAgeException extends Exception{
-    @Override
-    public String getMessage() {
-        return "You are not eligible because age is too high";
-    }
-}
-
 public class CustomExceptionExample {
     public static void main(String[] args) {
-
-
         try{
             Scanner scaner=new Scanner(System.in);
             System.out.println("Enter you age");
             int age=scaner.nextInt();
             checkAge(age);
-        } catch (UnderAgeException e) {
-            System.out.println(e.getMessage());
-        } catch (OverAgeException e) {
-            System.out.println(e.getMessage());
+        } catch (AgeException e) {
+            System.out.println("Not Eligible : "+e.getMessage());
         }
-
     }
-    public static void checkAge(int age) throws UnderAgeException,OverAgeException{
+    public static void checkAge(int age) {
         if (age<18){
-            throw new UnderAgeException();
+            throw new AgeException("Age is too low");
         }
         else if(age>60){
-            throw  new OverAgeException();
+            throw  new AgeException("Age is too high ");
         }
         else{
             System.out.println("You are eligible to apply");
